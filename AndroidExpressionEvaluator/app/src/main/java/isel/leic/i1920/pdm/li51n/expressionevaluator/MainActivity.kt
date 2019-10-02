c1e4eca47053f9aff1e88046f6ddf0a0aa3195db
Luís Falcão <lfalcao@cc.isel.pt>
2019-10-02 19:41:15 +0100

    Lesson 06: Activity lifecycle callback methods and multi-language support on Android Applications, using ExpressionEvaluator Application.

--- AndroidExpressionEvaluator/app/src/main/java/isel/leic/i1920/pdm/li51n/expressionevaluator/MainActivity.kt
+++ AndroidExpressionEvaluator/app/src/main/java/isel/leic/i1920/pdm/li51n/expressionevaluator/MainActivity.kt
@@ -7,9 +7,17 @@ import androidx.appcompat.app.AppCompatActivity
 import isel.leic.i1920.pdm.li51n.ExpressionEvaluator
 import kotlinx.android.synthetic.main.activity_main.*
 
+
+private val LOG_ID = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
     val EXPRESSION_VALUE = "RESULT"
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -19,13 +27,45 @@ class MainActivity : AppCompatActivity() {
             var expressionEvaluator = ExpressionEvaluator(expression.text.toString())
             result.text = "${expressionEvaluator.prettyPrint} = ${expressionEvaluator.value}"
         }
+
+        Log.i(LOG_ID, "##### onCreate called on ${this.hashCode()}")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(LOG_ID, "##### onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(LOG_ID, "##### onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(LOG_ID, "##### onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(LOG_ID, "##### onStop called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(LOG_ID, "##### onRestart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(LOG_ID, "##### onDestroy called")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(EXPRESSION_VALUE, result.text.toString())
 
-        Log.i("MainActivity", "##### onsaveInstanceState called")
+        Log.i(LOG_ID, "##### onSaveInstanceState called")
 
     }
 }
