8b78e1b50911dfdb46f78fb8845d896529340c18
Luís Falcão <lfalcao@cc.isel.pt>
2019-11-08 18:17:56 +0000

    Lesson 16: Using AsyncTask to parse Json data using Gson

--- Genuiz/app/src/main/java/isel/leic/i1920/pdm/li51n/utils/HttpRequests.kt
+++ Genuiz/app/src/main/java/isel/leic/i1920/pdm/li51n/utils/HttpRequests.kt
@@ -1,6 +1,8 @@
 package isel.leic.i1920.pdm.li51n.utils
 
 import android.content.Context
+import android.os.AsyncTask
+import android.util.Log
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.VolleyError
@@ -15,19 +17,40 @@ class HttpRequests(ctx: Context) {
 
     val gson = Gson()
 
-    inline fun <reified T> get(url: String, crossinline onSuccess: (T) -> Unit, crossinline onError: (AppError) -> Unit) {
+    inline fun <reified T> get(url: String, noinline onSuccess: (T) -> Unit, crossinline onError: (AppError) -> Unit) {
         // Request a string response from the provided URL.
         val stringRequest = StringRequest(
             Request.Method.GET,
             url,
             Response.Listener<String> { response ->
-                val dto = gson.fromJson(response, T::class.java)
-                onSuccess(dto)
+                println("onResponse thread is ${Thread.currentThread().id}")
+                val params = JsonParseAsyncTask.Params(gson, onSuccess, T::class.java, response)
+                JsonParseAsyncTask<T>().execute(params)
             },
             Response.ErrorListener { err -> onError(AppError(err))})
         // Add the request to the RequestQueue.
         queue.add(stringRequest)
-
     }
 
+
+
+    class JsonParseAsyncTask<T>: AsyncTask<JsonParseAsyncTask.Params<T>, Unit, T>() {
+
+        data class Params<T>(val gson:Gson, val onSuccess: (T) -> Unit, val javaKlass: Class<T>, val response: String)
+
+        private lateinit var onSuccess: (T) -> Unit
+
+        override fun doInBackground(vararg p: Params<T>): T {
+            val params = p[0]
+            onSuccess = params.onSuccess
+            println("doInBackground thread is ${Thread.currentThread().id}")
+            var dto = params.gson.fromJson(params.response, params.javaKlass)
+            return dto
+        }
+
+        override fun onPostExecute(result: T) {
+            println("onPostExecute thread is ${Thread.currentThread().id}")
+            onSuccess(result)
+        }
+    }
 }
